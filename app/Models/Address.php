de321a9533d35503f7d1a650550874c7840cf0d5
agostina ortellado <ortelladoagos@gmail.com>
2026-04-20 00:06:09 -0300

    refactor: update Address model definitions

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Address extends Model
+class Address extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable; 
+
     protected $fillable = [
         'town_id',
         'street',
